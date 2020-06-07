047eacb61ed78b7a8dbe78117c19c88062a34149
Nick Policastro <nickpoly48@gmail.com>
2020-06-07 13:31:52 -0400

    made changes to the entire repo

---
+++ assets/javascript/quizQuestions.js
@@ -0,0 +1,27 @@
+var quizQs = [
+    {
+        title: "NaN stands for: ",
+        choices: ["Numbers-aNonymous", "Not-any-Nannies", "Not-a-Number", "No-absolutely-Not"],
+        answer: "Not-a-Number"
+    },
+    {
+        title: "An array is enclosed in: ",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "square brackets"
+    },
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+      },
+      {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+      },
+    {
+        title: "This is a value that is passed back from one function to another: ",
+        choices: ["return", "class", "functionClass", "for"],
+        answer: "return"
+    },
+  ];
